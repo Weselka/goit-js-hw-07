8c65f9644f7eb97be29433a8e8037b5e6df28126
Weselka <weselyna.shvets@gmail.com>
2022-09-28 01:41:55 +0300

    task-2 copy temlate and render

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+let items = galleryItems;
+
+const galleryTemplate = ({ preview, original, description }) =>
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+
+const refs = {
+  containerGalleryItems: document.querySelector('.gallery'),
+};
+
+const render = () => {
+  const imgEl = items.map(galleryTemplate).join('');
+  refs.containerGalleryItems.insertAdjacentHTML('beforeend', imgEl);
+};
+
+render();
